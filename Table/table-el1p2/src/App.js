0ed30d4b52b3a0b7e1b822e75effaa931f88dbf6
Ezra <fansoaezra@gmail.com>
2022-06-16 00:57:39 +0300

    add: Table function, constants/dataTables.js and upload: App.css

--- Table/table-el1p2/src/App.js
+++ Table/table-el1p2/src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { dataTables } from './constants/dataTables';
+import { useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Table/>
   );
+}
+
+function Table() {
+  return (
+      <>
+          <div className="">
+              <h1>Tables</h1>
+          </div>
+          <div className='table-content'>
+            <TableHeader/>
+            <Tableau/>
+            <TableFooter/>
+          </div>
+      </>        
+  )
+}
+
+function TableHeader() {
+    return(
+      <>
+        <h4 className='table-title'>DataTable Exemple</h4>
+        <div className='select-form'>
+            <div className='select-area'>
+                <select id='selectPageEntries'>
+                    <option>10</option>
+                    <option>
+                        20
+                    </option>
+                    <option>30</option>
+                    <option>40</option>
+                    <option>50</option>
+                </select>
+                <label for='selectPageEntries'>entries per page</label>
+            </div>
+            <div>
+                <input type="search" placeholder='Search...'/>
+            </div>
+        </div>
+      </>
+    )
+}
+
+function Tableau() {
+    return (
+      <table >
+          <tr>
+              <th>Name</th>
+              <th>Position</th>
+              <th>Office</th>
+              <th>Age</th>
+              <th>Start Date</th>
+              <th>Salary</th>
+          </tr>
+          {dataTables.map((dataTable) => {
+              return <DataContent content={dataTable} />;
+              })} 
+      </table>
+    )
+}
+
+const DataContent = (props) => {
+  const { name, position, office, age, startDate, salary } = props.content;
+  return (
+      <tr>
+        <td>{name}</td>
+        <td>{position}</td>
+        <td>{office}</td>
+        <td>{age}</td>
+        <td>{startDate}</td>
+        <td>{salary}</td>
+      </tr>
+  )
+}
+
+function TableFooter() {
+  let [myData, setmyData] = useState(dataTables.length); 
+  return(
+    <div className='table-footer'>
+        <label>Show 1 to 10 of 57 entries</label>
+        <div>
+            <button onClick={() => {
+              setmyData(10);
+              console.log("tafa", {myData})
+              }}>
+                1
+            </button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+        </div>
+    </div>
+  )
 }
 
 export default App;
